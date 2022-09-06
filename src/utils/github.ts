7605db07e204f15d7186554ed74efbf2e95b5b8d
Matteo Colucci <matitald@gmail.com>
2022-09-07 00:27:15 +0200

    Support non existing files

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -1,44 +1,42 @@
 import { pinia } from '@/stores';
 import { useGithubSettings } from '@/stores/githubSettings';
-// import { Octokit } from '@octokit/rest';
+
+const BASE_URL = 'https://api.github.com'
 
 const settings = useGithubSettings(pinia);
 
-let cacheOctokit: { get:(url:string)=>Promise<Response> };
-const octokit = () => {
-  if (cacheOctokit) {
-    return cacheOctokit;
+let cacheApi: { get:(url:string)=>Promise<Response> };
+const api = () => {
+  if (cacheApi) {
+    return cacheApi;
   }
 
   if (!settings.token && !settings.repo) {
     return null;
   }
   
-  return cacheOctokit = (() => {
+  return cacheApi = (() => {
     const options = {
       headers: {
         Accept: 'application/vnd.github+json',
         Authorization: `Bearer ${settings.token}`,
       },
     };
-    const BASE_URL = 'https://api.github.com'
 
     return {
       get: (url:string) => fetch(`${BASE_URL}${url}`, { ...options, method: 'GET' })
     }
   })();
-  // return cacheOctokit = new Octokit({ 
-  //   auth: settings.token,
-  //   request: {
-  //     fetch: window.fetch,
-  //   }
-  // });
 }
 
 const b64_to_utf8 = (str:string) => decodeURIComponent(escape(window.atob(str)));
 
 export const fetchFileContent = async (filepath: string) => {
-  const res = await octokit()?.get(`/repos/${settings.repoUser}/${settings.repoName}/contents/${filepath}`);
+  const res = await api()?.get(`/repos/${settings.repoUser}/${settings.repoName}/contents/${filepath}`);
   const data = await res?.json();
+  console.log('data', data);
+  if (!data?.content) {
+    return null;
+  }
   return b64_to_utf8(data.content);
 };
